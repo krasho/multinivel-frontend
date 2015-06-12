bb2cd7e07f95589bd102c13bf41c134d8db8c756
Krasho <joseluis@balidevelop.com>
2015-06-12 15:17:04 -0400

    I used the tag ng-view for the view of bank

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,5 +1,17 @@
 (function(){
 var app = angular.module('multinivel', [
+   'ngRoute',
    'multinivel.BankController'
    ]);
+
+   app.config(['$routeProvider', function ($routeProvider) {
+
+     $routeProvider
+        .when('/', {
+            templateUrl: 'views/banks/index.html',
+            controller : 'BankController',
+            controllerAs: 'BankCtrl'
+        })
+   }])
+
 })();
